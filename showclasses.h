3f075ebbd380ef82a2665f1895568024719560dd
Youssef-Abo-El-Ela <youssefmm2003@gmail.com>
2024-02-13 22:34:53 +0200

    Revert "Revert "ShowPage""
    This reverts commit 4fd7ac94b5a440c50f856daed2a8c90482fdebc4.

---
+++ showclasses.h
@@ -0,0 +1,27 @@
+#ifndef SHOWCLASSES_H
+#define SHOWCLASSES_H
+
+#include "subject.h"
+#include <QWidget>
+
+static vector<Subject> subjects;
+namespace Ui {
+class ShowClasses;
+}
+
+class ShowClasses : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ShowClasses(QWidget *parent = nullptr);
+    ~ShowClasses();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::ShowClasses *ui;
+};
+
+#endif // SHOWCLASSES_H
